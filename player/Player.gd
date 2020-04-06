61f0a2960b7b1b8a36838cbb12f97a803148fd5b
Kevin Oubre <kjo018@latech.edu>
2020-04-05 21:28:16 -0500

    fixed sprite movement to make it rotational

--- player/Player.gd
+++ player/Player.gd
@@ -1,33 +1,34 @@
 extends KinematicBody2D
 
 var speed = 200
+var turn_speed = 3
+
+const ACCELERATION = Vector2(12,8)
+const MAX_VELOCITY = Vector2(800,40)
+
 var velocity = Vector2()
-onready var sprite = get_node("AnimatedSprite")
+var old_velocity = Vector2()
+onready var sprite = get_node("Body")
 
 
 func get_input():
-	
-	#look_at(get_global_mouse_position())
-	
+	old_velocity = velocity
 	velocity = Vector2()
-	
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-		sprite.play("walk_north")
-	
-	elif Input.is_action_pressed("left"):
-		velocity.x -= 1
-		sprite.play("walk_left")
-	elif Input.is_action_pressed("down"):
-		velocity.y += 1
-		sprite.play("walk_south")
-	elif Input.is_action_pressed("right"):
-		velocity.x += 1
-		sprite.play("walk_right")
-	else:
-		sprite.stop()
 
-	velocity = velocity.normalized() * speed 
+	if Input.is_action_pressed("up"):
+		velocity.y -= speed
+				
+	if Input.is_action_pressed("left"):
+		rotation_degrees -= turn_speed
+		
+	if Input.is_action_pressed("down"):
+		velocity.y += speed
+		
+	if Input.is_action_pressed("right"):
+		
+		rotation_degrees += turn_speed
+		
+	velocity = velocity.normalized().rotated(rotation) * speed 
 	move_and_slide(velocity)
 
 
