e568b3286b1ca3260c9e75786ec4bd1344d2702c
Colson <colsonthornton@gmail.com>
2020-05-17 23:05:14 -0500

    I don't even know what was changed

--- titlescreen/titlescreen.gd
+++ titlescreen/titlescreen.gd
@@ -3,6 +3,8 @@ extends Control
 var scene_path_to_load
 
 func _ready():
+	if Input.is_action_pressed("Escape"):
+		get_tree().quit()
 	for button in $menu/centerrow/buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
@@ -13,4 +15,8 @@ func _on_Button_pressed(scene_to_load):
 	
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
+
+func get_input():
+	if Input.is_action_pressed("Escape"):
+		get_tree().quit()
 	
